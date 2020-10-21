493a5d0850fa3e46da15480cae5edc4b00523f25
liberyu <liberyu@sina.com>
2020-10-21 16:30:21 +0800

    use pandas write csv

--- engine/inferencer.py
+++ engine/inferencer.py
@@ -37,27 +37,6 @@ def do_inference(
     sub.iloc[test_features['cp_type'] == 'ctl_vehicle', 1:] = 0
     sub.to_csv(infer_result_dir, index=False)
 
-    # f1 = open(submission_file, 'r', encoding='utf-8')
-    # f2 = open(infer_result_dir, 'w', encoding='utf-8', newline='')
-    #
-    #
-    # contents = f1.readlines()
-    #
-    # csv_writer = csv.writer(f2)
-    #
-    # csv_writer.writerow(contents.pop(0).split(','))
-
-    # for i in trange(len(categories)):
-    #     cat = torch.tensor([categories[i]])
-    #     num = torch.tensor([numerical[i]], dtype=torch.float32)
-    #     pred1, pred2 = model(cat, num)
-    #     pred1 = torch.sigmoid(pred1).detach().numpy()[0].tolist()
-    #     pred1.insert(0, contents[i].split(',')[0])
-    #     csv_writer.writerow(pred1)
-    #     pass
-    #
-    # f1.close()
-    # f2.close()
     pass
 
 
